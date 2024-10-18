c4158ba85643e87e9a9f3bcc9a9d00cb79b517a5
Hexex36 <matej@sloupovi.info>
2024-10-18 22:38:00 +0200

    Semi-function script prototype

--- visualizer.py
+++ visualizer.py
@@ -2,12 +2,13 @@ import streamlit as st
 import polars as pl
 import folium as fo
 from streamlit_folium import st_folium
+from typing import Dict, List
 
 # Debug data
 try:
-    schools = pl.read_excel("schools.xlsx")
+    schools_source = pl.read_excel("schools.xlsx")
 except:
-    schools = pl.DataFrame({
+    schools_source = pl.DataFrame({
         "name":["School 1", "School 2", "School 3", "School 4"],
         "country":["Spain", "Spain", "Czechia", "Mars"],
         "study":["Maths", "Chemistry", "Maths", "Biology"],
@@ -15,8 +16,43 @@ except:
         "longtitude":[14.25, 22.74, 37.8, 44.444]
         })
 
+schools:pl.DataFrame = schools_source.select("name", "country", "study")
+picks = schools.to_dict()
+for column in picks.keys():
+    picks[column] = ["---"]  + picks[column].unique(maintain_order=True).to_list()
+
+def filter_schools(school_df:pl.DataFrame) -> pl.DataFrame:
+    filtered = school_df
+    filter_choice = []
+
+    for column in filtered.columns:
+        if column not in st.session_state:
+            continue
+
+        if len(st.session_state[column]) > 0:
+            filter_choice.append(pl.col(column).is_in(st.session_state[column])) 
+        
+    if len(filter_choice) < 1:
+        filter_choice = [True]
+    print(filter_choice)
+
+    return filtered.filter(filter_choice)
+
 st.header("UJEP PRF Erasmus")
 st.divider()
+
+# --- TABULKA ---
+
+filters = st.columns(len(schools.columns))
+for index, column in enumerate(schools.columns):
+    with filters[index]:
+        #picks = schools_source.to_series(schools.get_column_index(column)).to_list()
+        st.session_state[column] = st.multiselect(label=column, options=picks[column])
+
+schools = filter_schools(schools_source)
+schools_sub = schools.select("name", "country", "study")
+
+st.table(schools_sub)
 
 # --- MAPA ---
 # Není tam možnost zobrazení informací o škole když se tam najede myší, takže jdeme dělat funky shit
@@ -38,6 +74,13 @@ europe = fo.Map(
     max_lon=max_long
 )
 
+coords = zip(schools.to_series(schools.get_column_index("name")).to_list(),schools.to_series(schools.get_column_index("latitude")).to_list(),schools.to_series(schools.get_column_index("longtitude")).to_list())
+for coord in coords:
+    fo.Marker(
+        location=[coord[1], coord[2]],
+        popup=fo.Popup(coord[0])
+    ).add_to(europe)
+
 # Body ilustrující kam až lze tahat "kameru" (debug only)
 fo.CircleMarker([max_lat, max_long]).add_to(europe)
 fo.CircleMarker([max_lat, min_long]).add_to(europe)
@@ -47,6 +90,4 @@ fo.CircleMarker([min_lat, min_long]).add_to(europe)
 # Spuštění (thank you based open-source contributors)
 st_folium(europe, use_container_width=True)
 
-# --- TABULKA ---
-st.table(schools.select("name", "country", "study"))
-
+st.session_state
